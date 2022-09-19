1754eadbbff18f939263a593c93bda3c0f8f8e31
QUANGNT\quang <quangnt1702@gmail.com>
2022-09-19 22:38:59 +0700

    feat: add error filter

---
+++ NTQ.Sdk.Core/Extensions/ErrorFilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NTQ.Sdk.Core.Filters;
+
+namespace NTQ.Sdk.Core.Extensions
+{
+    public static class ErrorHandlingFilterConfig
+    {
+        public static void ConfigErrorHandlingFilter(this IServiceCollection services)
+        {
+            services.AddControllers(options => options.Filters.Add<ErrorHandlingFilter>());
+        }
+    }
+}
